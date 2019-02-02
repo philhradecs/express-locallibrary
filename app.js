cc21ab898726d8921120ef2f28029ab18581ea2b
Philip Hradecsni <baumzeit@posteo.me>
2019-02-02 19:18:26 +0100

    create mongoose schemas

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://admin:local_library_01@ds119795.mlab.com:19795/local_library';
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
